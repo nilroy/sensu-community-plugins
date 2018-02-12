83a3242932ced29c7ba1aa4f7a8aa582c2b095cc
Nilanjan Roy <nilanjan.roy@nosto.com>
2018-02-12 13:31:09 +0200

    using the upstream version

--- plugins/influxdb/check-influxdb-query.rb
+++ plugins/influxdb/check-influxdb-query.rb
@@ -35,8 +35,11 @@ require 'json'
 require 'jsonpath'
 require 'dentaku'
 
-VERSION = '0.1.0'
+# VERSION = '0.1.0'
 
+#
+# Check Influxdb Query
+#
 class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
   check_name nil
   option :host,
@@ -51,11 +54,37 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
          default: '8086',
          description: 'InfluxDB port'
 
+  option :use_ssl,
+         description: 'Turn on/off SSL (default: false)',
+         short: '-s',
+         long: '--use_ssl',
+         boolean: true,
+         default: false
+
+  option :verify_ssl,
+         description: 'Turn on/off using SSL certificate (default: false)',
+         short: '-v',
+         long: '--verify_ssl',
+         boolean: true,
+         default: false
+
+  option :ssl_ca_cert,
+         description: 'Path to the ssl ca certificate to connect to the InfluxDB server',
+         short: '-C CA_CERT',
+         long: '--ssl_ca_cert CA_CERT'
+
   option :database,
          short: '-d DATABASE',
          long: '--database DATABASE',
          default: 'influxdb',
          description: 'InfluxDB database name'
+
+  option :retry,
+         short: '-r RETRY',
+         long: '--retry RETRY',
+         description: 'InfluxDB retry count with exponential back-off',
+         proc: proc(&:to_i),
+         default: 12
 
   option :username,
          short: '-u USERNAME',
@@ -73,13 +102,19 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
          short: '-q QUERY',
          long: '--query QUERY',
          required: true,
-         description: 'Query to run. See http://influxdb.com/docs/v0.8/api/query_language.html'
+         description: 'Query to run. See https://influxdb.com/docs/v0.9/query_language/query_syntax.html'
 
   option :alias,
          short: '-a ALIAS',
          long: '--alias ALIAS',
          default: nil,
          description: 'Alias of query (e.g. if query and output gets too long)'
+
+  option :mode,
+         short: '-m MODE',
+         long: '--mode MODE',
+         default: 'first',
+         description: 'How the results are being checked (one of "first", "last", "max", "min", "avg") when the query returns more than one value'
 
   option :jsonpath,
          short: '-j JSONPATH',
@@ -114,73 +149,81 @@ class CheckInfluxdbQuery < Sensu::Plugin::Check::CLI
          show_options: true,
          exit: 0
 
-  option :version,
-         short: '-v',
-         long: '--version',
-         description: 'Show version',
-         on: :tail,
-         boolean: true,
-         proc: proc { puts "Version #{VERSION}" },
-         exit: 0
+  # option :version,
+  #        short: '-v',
+  #        long: '--version',
+  #        description: 'Show version',
+  #        on: :tail,
+  #        boolean: true,
+  #        proc: proc { puts "Version #{VERSION}" },
+  #        exit: 0
 
   def run
     influxdb = InfluxDB::Client.new config[:database],
                                     host: config[:host],
                                     port: config[:port],
+                                    use_ssl: config[:use_ssl],
+                                    verify_ssl: config[:verify_ssl],
+                                    ssl_ca_cert: config[:ssl_ca_cert],
+                                    retry: config[:retry],
                                     username: config[:username],
                                     password: config[:password]
 
     value = influxdb.query config[:query]
-    values = value.each_slice(1).to_a
 
-    if config[:alias]
-      query_name = config[:alias]
-    else
-      query_name = config[:query]
-    end
+    query_name = if config[:alias]
+                   config[:alias]
+                 else
+                   config[:query]
+                 end
 
     if config[:noresult] && value.empty?
       critical "No result for query '#{query_name}'"
+    elsif config[:noresult] && !config[:jsonpath] && !value.empty?
+      ok "Value returned for query '#{query_name}'"
     end
 
     if config[:jsonpath]
       json_path = JsonPath.new(config[:jsonpath])
       calc = Dentaku::Calculator.new
-      _critical_ = []
-      _warning_ = []
-      _ok_ = []
-      values.each do |_value_|
-        value = json_path.on(_value_).first || 0
-        if config[:critical] && calc.evaluate(config[:critical], value: value)
-          _critical_ << {"value" => value, "tags" => _value_[0]['tags']}
-        elsif config[:warning] && calc.evaluate(config[:warning], value: value)
-          _warning_ << {"value" => value, "tags" => _value_[0]['tags']}
-        else
-          _ok_ << {"value" => value, "tags" => _value_[0]['tags'] }
+      if config[:mode] == 'any' && json_path.on(value).length >= 1
+        json_path.on(value).each do |hashval|
+          if config[:critical] && calc.evaluate(config[:critical], value: hashval)
+            critical "Value '#{value}' matched '#{config[:critical]}' for query '#{query_name}'"
+          elsif config[:warning] && calc.evaluate(config[:warning], value: hashval)
+            warning "Value '#{value}' matched '#{config[:warning]}' for query '#{query_name}'"
+          end
         end
-      end
-      if ! _critical_.empty?
-        if _critical_.count > 1
-          critical "Multiple values '#{_warning_}' matched '#{config[:critical]}' for query '#{query_name}'"
-        else
-          critical "Value '#{_critical_[0]["value"]}' ('tags'=>#{_critical_[0]['tags']}) matched '#{config[:critical]}' for query '#{query_name}'"
-        end
-      elsif ! _warning_.empty?
-        if _warning_.count > 1
-          warning "Multiple values '#{_warning_}' matched '#{config[:warning]}' for query '#{query_name}'"
-        else
-          warning "Value '#{_warning_[0]["value"]}' ('tags'=>#{_warning_[0]['tags']}) matched '#{config[:warning]}' for query '#{query_name}'"
-        end
+        ok 'All values OK!'
       else
-        if _ok_.count > 1
-          ok "Multiple values '#{_ok_}' ok for query '#{query_name}'"
+        value = get_single_value(json_path, value)
+        if config[:critical] && calc.evaluate(config[:critical], value: value)
+          critical "Value '#{value}' matched '#{config[:critical]}' for query '#{query_name}'"
+        elsif config[:warning] && calc.evaluate(config[:warning], value: value)
+          warning "Value '#{value}' matched '#{config[:warning]}' for query '#{query_name}'"
         else
-          ok "Value '#{_ok_[0]["value"]}' ('tags'=>#{_ok_[0]['tags']}) ok for query '#{query_name}'"
+          ok "Value '#{value}' ok for query '#{query_name}'"
         end
       end
     else
       puts 'Debug output. Use -j to check value...'
-      puts JSON.pretty_generate(values)
+      puts JSON.pretty_generate(value)
+    end
+  end
+
+  def get_single_value(jpath, value)
+    return 0 if jpath.on(value).empty?
+    case config[:mode]
+    when 'last'
+      jpath.on(value).last
+    when 'min'
+      jpath.on(value).min
+    when 'max'
+      jpath.on(value).max
+    when 'avg', 'average'
+      jpath.on(value).inject(:+).to_f / jpath.on(value).length
+    else
+      jpath.on(value).first
     end
   end
 end
